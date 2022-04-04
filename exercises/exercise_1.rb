dd9a38fe2a26250d6a057639484ac55ddf36283c
trijaychan <trijaychan@gmail.com>
2022-04-04 17:43:57 +0000

    Finished exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,25 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create(
+  name: "Burnaby",
+  annual_revenue: 300000,
+  mens_apparel: 1,
+  womens_apparel: 1
+)
+
+Store.create(
+  name: "Richmond",
+  annual_revenue: 1260000,
+  mens_apparel: 0,
+  womens_apparel: 1
+)
+
+Store.create(
+  name: "Gastown",
+  annual_revenue: 190000,
+  mens_apparel: 1,
+  womens_apparel: 0
+)
+
+puts "#{Store.count} store(s) in the Store Database"
